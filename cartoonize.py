4329b0b4e78034c011cca74e3ab2b090bdd160ec
SystemErrorWang <Secret_Wang@outlook.com>
2019-06-20 14:29:49 +0800

    update with runable model

---
+++ cartoonize.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as numpy
+import tensorflow as tf 
+
+
+def cartoonize(image_path):
+	interpreter = tf.contrib.lite.Interpreter(model_path="cartoon_gan.tflite")
+	interpreter.allocate_tensors()
+
+	# Get input and output tensors.
+	input_details = interpreter.get_input_details()
+	output_details = interpreter.get_output_details()
+
+	# Test model on random input data.
+	input_shape = input_details[0]['shape']
+	image = cv2.imread(image_path)
+	image = cv2.resize(image, (256, 256),
+						interpolation=cv2.INTER_CUBIC)
+	image = image.astype(np.float32)/127.5 - 1
+	image = image.resize(input_shape)
+	interpreter.set_tensor(input_details[0]['index'], image)
+
+	interpreter.invoke()
+	output = interpreter.get_tensor(output_details[0]['index'])
+	output = (np.squeeze(output)+1)*127.5
+	output = np.clip(output, 0, 255)
+	cv2.imwrite('cartoon_image.jpg', output)
+
+
+if __name__ == '__main__':
+	image_path = 'test_image.jpg'
+	cartoonize(image_path)
